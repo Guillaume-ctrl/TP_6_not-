100863aa0bf6bd0a847e4f653562d6b6d7e383fe
Frag <frag@vtd.com>
2025-01-23 14:58:43 +0100

    Added next and previous Case method

--- TP6/Case.cpp
+++ TP6/Case.cpp
@@ -1,6 +1,6 @@
 #include "Case.hpp"
 
-Case::Case(const std::string& name) : m_name(name), m_taxe(0), m_couleur(ROUGE)
+Case::Case(const std::string& name) : m_name(name)
 {
 
 }
@@ -8,4 +8,24 @@ Case::Case(const std::string& name) : m_name(name), m_taxe(0), m_couleur(ROUGE)
 inline std::string Case::getName() const
 {
 	return this->m_name;
+}
+
+Case* Case::getNextCase() const
+{
+	return this->m_next_case;
+}
+
+Case* Case::getPreviousCase() const
+{
+	return this->m_previous_case;
+}
+
+void Case::setNextCase(Case* _case)
+{
+	this->m_next_case = _case;
+}
+
+void Case::setPreviousCase(Case* _case)
+{
+	this->m_previous_case = _case;
 }
--- TP6/Case.hpp
+++ TP6/Case.hpp
@@ -1,48 +1,68 @@
+/**
+ * @file Case.hpp
+ * @brief Declaration of the Case class for the game board.
+ */
+
+#pragma once
 #include <string>
 #include "Player.hpp"
 
-/**
- * @brief base class of a Case
- */
-
-enum Couleur {
-	ROUGE     = 0x00,
-	BLEU      = 0x01,
-	CIAN      = 0x02,
-	VERT      = 0x03,
-	MAUVE     = 0x04,
-	ROSE      = 0x05,
-	ORANGE    = 0x06,
-	JAUNE     = 0x07,
-};
-
+ /**
+  * @class Case
+  * @brief Represents a case (tile) on the game board.
+  */
 class Case
 {
 public:
-	/**
-	 * @brief Constructor
-	 * @param name Name of the case
-	 */
-	Case(const std::string& name);
+    /**
+     * @brief Constructor for the Case class.
+     * @param name Name of the case.
+     */
+    Case(const std::string& name);
 
-	/**
-	 * @brief Get the name of the case
-	 * @return the name of the case
-	 */
-	std::string getName() const;
+    /**
+     * @brief Retrieves the name of the case.
+     * @return The name of the case as a std::string.
+     */
+    std::string getName() const;
 
-	/**
-	 * @brief Virtual method called upon a player landing
-	 * @param landingPlayer the player that landed
-	 */
-	virtual void onLanding(Player* landingPlayer) = 0;
+    /**
+     * @brief Virtual method called when a player lands on this case.
+     * @param landingPlayer Pointer to the player who landed on this case.
+     * This method must be implemented by derived classes.
+     */
+    virtual void onLanding(Player* landingPlayer) = 0;
+
+    /**
+     * @brief Retrieves the next case in the sequence.
+     * @return Pointer to the next case, or nullptr if none exists.
+     */
+    Case* getNextCase() const;
+
+    /**
+     * @brief Retrieves the previous case in the sequence.
+     * @return Pointer to the previous case, or nullptr if none exists.
+     */
+    Case* getPreviousCase() const;
+
+    /**
+     * @brief Sets the next case in the sequence.
+     * @param _case Pointer to the next case.
+     */
+    void setNextCase(Case* _case);
+
+    /**
+     * @brief Sets the previous case in the sequence.
+     * @param _case Pointer to the previous case.
+     */
+    void setPreviousCase(Case* _case);
 
 private:
-	/**
-	 * @brief Name of the player
-	 */
-	std::string m_name;
-	unsigned int m_taxe;
-	Couleur m_couleur;
+    Case* m_next_case;     /**< Pointer to the next case in the sequence. */
+    Case* m_previous_case; /**< Pointer to the previous case in the sequence. */
 
+    /**
+     * @brief Name of the case.
+     */
+    std::string m_name;
 };
